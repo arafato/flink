568fc3bd6e06d5a2809ec1efae515af2a7628faf
arafato <oliver.arafat@outlook.com>
2016-11-22 16:41:55 +0100

    extended constructor

--- flink-streaming-connectors/flink-connector-eventhub/src/main/java/org/apache/flink/streaming/connectors/eventhub/internals/EventHubDataFetcher.java
+++ flink-streaming-connectors/flink-connector-eventhub/src/main/java/org/apache/flink/streaming/connectors/eventhub/internals/EventHubDataFetcher.java
@@ -1,20 +1,39 @@
 package org.apache.flink.streaming.connectors.eventhub.internals;
 
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.eventhub.models.EventHubPartition;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventHubDataFetcher {
+public class EventHubDataFetcher<T> {
 	private List<EventHubPartition> subscribedPartitions = new ArrayList<>();
+
+	private final SourceFunction.SourceContext<T> sourceContext;
+
+	/** Checkpoint lock, also used to synchronize operations on subscribedShardsState */
+	private final Object checkpointLock;
 
 	private EventHubManager eventHubManager;
 	private LeaseManager leaseManager;
 
 	public EventHubDataFetcher(EventHubManager eventHubManager,
-							   LeaseManager leaseManager) {
+							   LeaseManager leaseManager,
+							   SourceFunction.SourceContext sourceContext) {
+		this(eventHubManager,
+			 leaseManager,
+			 sourceContext,
+			 sourceContext.getCheckpointLock());
+	}
+
+	public EventHubDataFetcher(EventHubManager eventHubManager,
+							   LeaseManager leaseManager,
+							   SourceFunction.SourceContext sourceContext,
+							   Object checkpointLock) {
 		this.eventHubManager = eventHubManager;
 		this.leaseManager = leaseManager;
+		this.sourceContext = sourceContext;
+		this.checkpointLock = checkpointLock;
 		this.populateSubscribedPartitions();
 	}
 	
